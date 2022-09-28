5c619d524b07c14e3a92d33b1c8399d348e05ad7
Sofía Escudero <ssfescudero@gmail.com>
2022-09-27 22:49:15 -0300

    Add files via upload

---
+++ ejercicioparcial2.sql
@@ -0,0 +1,55 @@
+﻿/*==============================================================*/
+/*  DROPS: Descomentar tras ejecutar el script por primera vez. */
+/*==============================================================*/
+
+drop table persona;
+drop table localidad;
+drop table provincia;
+
+/*==============================================================*/
+/* 			    NIVEL 1                             */
+/*==============================================================*/
+
+create table provincia (
+	id			int		not null,
+	codigo_provincia	smallint	not null,
+	nombre			char(60)	not null,
+	constraint pk_provincia primary key (id),
+	constraint uk_provincia UNIQUE (codigo_provincia)
+);
+
+/*==============================================================*/
+/* 			    NIVEL 2                             */
+/*==============================================================*/
+
+create table localidad (
+	id			int		not null,
+	id_provincia		smallint	not null,
+	codigo_localidad	smallint	not null,
+	nombre			char(60)	not null,
+	constraint pk_localidad primary key (id,id_provincia),
+	constraint uk_localidad UNIQUE (codigo_localidad),
+	constraint fk_localidad_references_provincia foreign key (id_provincia) references provincia(id)
+);
+
+/*==============================================================*/
+/* 			    NIVEL 3                             */
+/*==============================================================*/
+
+create table persona (
+	id			int		not null,
+	tipoDocumento		varchar		not null,
+		constraint chk_tipoDocumento CHECK (tipoDocumento in ('D', 'LE', 'LC', 'P', 'NI')),
+	numeroDocumento		int		not null,
+	id_localidad	smallint,
+	id_provincia	smallint,
+	apellido		char(120)	not null,
+	nombre			char(120)	not null,
+	fechaNacimiento		date,
+	sexo			char(1)		not null,
+		constraint chk_sexo CHECK (sexo in ('F','M','N')),
+	observaciones		varchar,
+	constraint pk_persona primary key (id),
+	constraint uk_persona UNIQUE (tipoDocumento,numeroDocumento),
+	constraint fk_persona_references_localidad foreign key (id_localidad,id_provincia) references localidad (id,id_provincia)
+);
---
+++ ejercicioparcial3.sql
@@ -0,0 +1,52 @@
+﻿/*==============================================================*/
+/*  			     DROPS 				*/
+/*==============================================================*/
+
+ALTER TABLE persona
+	drop constraint pk_persona;
+ALTER TABLE persona
+	drop constraint fk_persona_references_localidad;
+	
+ALTER TABLE localidad
+	drop constraint pk_localidad;
+ALTER TABLE localidad
+	drop constraint fk_localidad_references_provincia;
+	
+ALTER TABLE provincia
+	drop constraint pk_provincia;
+
+/*==============================================================*/
+/*  			    CAMBIOS 				*/
+/*==============================================================*/
+
+ALTER TABLE provincia -- agrego id
+	add column id 	int 	not null;
+ALTER TABLE provincia -- actualizo la primary key
+	add constraint pk_provincia primary key (id);
+ALTER TABLE provincia -- seteo la clave alternativa
+	add constraint uk_provincia UNIQUE (codigo_provincia);
+	
+ALTER TABLE localidad -- agrego id
+	add column id	int 	not null;
+ALTER TABLE localidad -- cambio nombre de la pk heredada
+	rename codigo_provincia TO id_provincia;
+ALTER TABLE localidad -- actualizo la primary key
+	add constraint pk_localidad primary key (id,id_provincia);
+ALTER TABLE localidad -- seteo la clave alternativa
+	add constraint uk_localidad UNIQUE (codigo_localidad);
+ALTER TABLE localidad -- agrego foreign key
+	add constraint fk_localidad_references_provincia foreign key (id_provincia) references provincia(id);
+
+ALTER TABLE persona -- agrego id
+	add column id 	int 	not null;
+ALTER TABLE persona -- cambio nombre de la fk
+	rename codigo_localidad to id_localidad;
+ALTER TABLE persona  -- cambio nombre de la fk
+	rename codigo_provincia to id_provincia;
+ALTER TABLE persona -- actualizo la primary key
+	add constraint pk_persona primary key (id);
+ALTER TABLE persona -- agrego clave alternativa
+	add constraint uk_persona UNIQUE (tipoDocumento,numeroDocumento);
+ALTER TABLE persona -- agrego nueva foreign key con ids
+	add constraint fk_persona_references_localidad foreign key (id_localidad,id_provincia) references localidad(id,id_provincia);
+	
